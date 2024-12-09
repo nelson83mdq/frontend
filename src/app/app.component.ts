9e06b6c712fb2485e6ad8754826f13ae13401949
= <dbrizuela@zoonsuite.com>
2024-12-08 23:12:55 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './servicios/api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  message: any;
+
+
+  constructor(private api: ApiService){
+    this.api.getMessage().subscribe(msg=>{
+      this.message = msg;
+      console.log(msg);
+      
+    })
+  }
 }
